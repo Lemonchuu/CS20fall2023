b92004887785f72664d4b4775051215813fce468
47215501 <47215501@L815WD220033.edu.cbe.ab.ca>
2023-09-12 09:43:35 -0600

    9:43am

---
+++ demo 1/src/Test1.java
@@ -0,0 +1,30 @@
+
+public class Test1 {
+
+	public static void main(String[] args){
+		 
+		 //System.out.print("Hi!\n\t"); 
+		 
+		 //Declaration
+		 int age;
+		  double cost;
+		  String name;
+		  char letter;
+		 //initialization
+		 age = 16;
+		 cost = 44.50;
+		 name = "Rain";
+		 letter ='N';
+		 System.out.print((age+cost)+ "=" +name);
+		 
+		   
+	
+	}
+		
+	
+
+   
+	
+	
+	
+	}
